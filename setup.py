9ff41f0e2a37aecc0a982ee3c543cafaa2915a4f
remokasu <0w0.ebi.kaitai@gmail.com>
2023-04-21 02:12:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
+from codecs import open
+from os import path
+
 from setuptools import find_packages, setup
 
+here = path.abspath(path.dirname(__file__))
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('LICENSE', 'r', encoding='utf-8') as f:
+    license = f.read()
+
 setup(
-    name="stacker",
-    version="1.0.1",
+    author='remokasu',
+    name="pystacker",
+    version="1.0.3",
+    license=license,
+    url='https://github.com/remokasu/stacker',
     packages=find_packages(),
     package_data={'stacker': ['data/*']},
+    description='Stacker: RPN Calculator in Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='reverse-polish-calculator rpn terminal-app',
     entry_points={
         "console_scripts": [
             "stacker = stacker.stacker:main",
