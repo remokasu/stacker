b16bc207bdbe2fc629270ce0c8404807abb47570
remokasu <0w0.ebi.kaitai@gmail.com>
2024-10-03 22:58:33 +0900

    fix lambda

--- stacker/lib/function/lmd.py
+++ stacker/lib/function/lmd.py
@@ -4,14 +4,6 @@ from typing import TYPE_CHECKING
 from stacker.slambda import StackerLambda
 
 
-if TYPE_CHECKING:
-    from stacker.stacker import Stacker
-
-
-def _lambda(stacker: Stacker, fargs: list, body: Stacker) -> None:
-    return StackerLambda(fargs, body)
-
-
 lambda_operators = {
     "lambda": {
         "func": lambda fargs, body: StackerLambda(fargs, body),
